1c96ded4e3ebbcee251b798748213fc2482a3454
Sergey Cherednichenko <sergey.cherednichenko@simbirsoft.com>
2020-03-18 17:37:00 +0300

    Запущен каталог

---
+++ storage/modification/catalog/controller/common/menu.php
@@ -0,0 +1,87 @@
+<?php
+class ControllerCommonMenu extends Controller {
+	public function index() {
+		$this->load->language('common/menu');
+
+        $data['manufacturer'] = $this->url->link('product/manufacturer');
+        $data['contact'] = $this->url->link('information/contact');
+        $data['special'] = $this->url->link('product/special');
+        $data['search'] = $this->load->controller('common/search');
+        $data['logged'] = $this->customer->isLogged();
+		$data['account'] = $this->url->link('account/account', '', true);
+		$data['register'] = $this->url->link('account/register', '', true);
+		$data['login'] = $this->url->link('account/login', '', true);
+		$data['order'] = $this->url->link('account/order', '', true);
+		$data['transaction'] = $this->url->link('account/transaction', '', true);
+		$data['download'] = $this->url->link('account/download', '', true);
+		$data['logout'] = $this->url->link('account/logout', '', true);
+		$data['language'] = $this->load->controller('common/language');
+		$data['currency'] = $this->load->controller('common/currency');
+        $data['wishlist'] = $this->url->link('account/wishlist', '', true);
+        $data['compare'] = $this->url->link('product/compare');
+        $data['text_compare'] = sprintf($this->language->get('text_compare'), (isset($this->session->data['compare']) ? count($this->session->data['compare']) : 0));
+				if ($this->customer->isLogged()) {
+			$this->load->model('account/wishlist');
+
+			$data['text_wishlist'] = sprintf($this->language->get('text_wishlist'), $this->model_account_wishlist->getTotalWishlist());
+		} else {
+			$data['text_wishlist'] = sprintf($this->language->get('text_wishlist'), (isset($this->session->data['wishlist']) ? count($this->session->data['wishlist']) : 0));
+		}		
+		$data['informations'] = array();
+
+		foreach ($this->model_catalog_information->getInformations() as $result) {
+			if ($result['bottom']) {
+				$data['informations'][] = array(
+					'title' => $result['title'],
+					'href'  => $this->url->link('information/information', 'information_id=' . $result['information_id'])
+				);
+			}
+		}
+		
+		if ($this->config->get('configblog_blog_menu')) {
+			$data['blog_menu'] = $this->load->controller('blog/menu');
+		} else {
+			$data['blog_menu'] = '';
+		}
+
+		// Menu
+		$this->load->model('catalog/category');
+
+		$this->load->model('catalog/product');
+
+		$data['categories'] = array();
+
+		$categories = $this->model_catalog_category->getCategories(0);
+
+		foreach ($categories as $category) {
+			if ($category['top']) {
+				// Level 2
+				$children_data = array();
+
+				$children = $this->model_catalog_category->getCategories($category['category_id']);
+
+				foreach ($children as $child) {
+					$filter_data = array(
+						'filter_category_id'  => $child['category_id'],
+						'filter_sub_category' => true
+					);
+
+					$children_data[] = array(
+						'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
+					);
+				}
+
+				// Level 1
+				$data['categories'][] = array(
+					'name'     => $category['name'],
+					'children' => $children_data,
+					'column'   => $category['column'] ? $category['column'] : 1,
+					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
+				);
+			}
+		}
+
+		return $this->load->view('common/menu', $data);
+	}
+}
---
+++ storage/modification/catalog/language/en-gb/common/menu.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_all'] = 'Show All';
+		
+		$_['text_compare']      = 'Product Compare (%s)';
+     
